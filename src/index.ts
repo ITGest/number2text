51e809de2f7c2ea9a67948711b1b6405a489de16
Adelino Safeca <safeetyy@gmail.com>
2020-06-19 01:50:24 +0100

    package.json confg

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { NumberService } from './components/service/number_service';
 /// THIS IS THE MAIN FUNCTION, CALL IT TO CONVERT NUMBRS
-export function NumberToString(value: number): string {
+export function getText(value: number): string {
   if (value >= 1000000000000000000 || value < 0) {
     return 'Not supported number';
   } else if (value === 0) return 'ZERO';
